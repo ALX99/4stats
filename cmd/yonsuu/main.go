621a2768dfa8d7923d85996e03170660f09e473d
ALX99 <46844683+ALX99@users.noreply.github.com>
2023-01-25 20:53:15 +0100

    chore: refactor code improvements

--- cmd/yonsuu/main.go
+++ cmd/yonsuu/main.go
@@ -1,13 +1,27 @@
 package main
 
 import (
+	"context"
+	"log"
+	"os/signal"
+	"syscall"
+
 	"github.com/alx99/yonsuu/internal/yonsuu/board"
 	"github.com/alx99/yonsuu/internal/yonsuu/metrics"
 )
 
 func main() {
 	m := metrics.New()
-	m.Start()
+	if err := m.Start(); err != nil {
+		log.Fatalln(err)
+	}
+
 	b := board.New("po")
-	b.Watch()
+	if err := b.StartWatch(); err != nil {
+		log.Fatalln(err)
+	}
+
+	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	<-ctx.Done()
+	cancel()
 }
--- internal/yonsuu/board/board.go
+++ internal/yonsuu/board/board.go
@@ -2,15 +2,22 @@ package board
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
-	"log"
 	"net/http"
+	"sync"
+	"sync/atomic"
 	"time"
 )
 
 type Board struct {
-	name string
+	name   string
+	apiURL string
+
+	running atomic.Bool
+	stop    chan any
+	sync.WaitGroup
 }
 
 type boardThreads []struct {
@@ -23,34 +30,71 @@ type boardThreads []struct {
 }
 
 func New(name string) Board {
-	return Board{name: name}
+	return Board{
+		name:   name,
+		apiURL: fmt.Sprintf("https://a.4cdn.org/%s/catalog.json", name),
+	}
 }
 
-func (b *Board) Watch() {
-	threadsAPI := fmt.Sprintf("https://a.4cdn.org/%s/catalog.json", b.name)
+func (b *Board) StartWatch() error {
+	b.stop = make(chan any)
+	b.Add(1)
+	delay := time.Minute
 
-	for {
-		resp, err := http.Get(threadsAPI)
-		if err != nil {
-			log.Println(err)
-			continue
-		}
-
-		// Read the response body
-		body, err := io.ReadAll(resp.Body)
-		if err != nil {
-			log.Println(err)
-			continue
-		}
-		resp.Body.Close()
-
-		threads := boardThreads{}
-		if err = json.Unmarshal(body, &threads); err != nil {
-			log.Println(err)
-			continue
-		}
-
-		fmt.Printf("%+v", threads)
-		time.Sleep(time.Minute)
+	if b.running.Swap(true) {
+		return errors.New("board is already running")
 	}
+
+	go func() {
+		ticker := time.NewTicker(delay)
+		defer b.Done()
+		defer ticker.Stop()
+
+		for {
+			if err := b.refresh(); err != nil {
+				fmt.Println(err)
+			}
+
+			ticker.Reset(delay)
+			select {
+			case <-ticker.C:
+				if err := b.refresh(); err != nil {
+					fmt.Println(err)
+				}
+
+			case <-b.stop:
+				return
+			}
+		}
+	}()
+
+	return nil
+}
+
+// refresh calls the API and refreshes the statistics
+func (b *Board) refresh() error {
+	resp, err := http.Get(b.apiURL)
+	if err != nil {
+		return err
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+	resp.Body.Close()
+
+	threads := boardThreads{}
+	if err = json.Unmarshal(body, &threads); err != nil {
+		return err
+	}
+
+	fmt.Printf("%+v", threads)
+
+	return nil
+}
+
+func (b *Board) StopWatch() {
+	close(b.stop)
+	b.Wait()
 }
